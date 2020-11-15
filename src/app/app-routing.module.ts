1e9ab17df8e77e0a08ea0eabc2797d1fc7dc728c
ks <anandhuks1027@gmail.com>
2020-11-15 21:05:16 +0530

    new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OutputComponent } from './output/output.component';
+import { QuilleditorComponent } from './quilleditor/quilleditor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:QuilleditorComponent},
+  {path:"output",component:OutputComponent},
+  {path:"editor",component:QuilleditorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { QuilleditorComponent } from './quilleditor/quilleditor.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuilleditorComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    QuillModule.forRoot(),
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
