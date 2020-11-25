45cc76d2f5a14ffe38fa646936558b469915e04b
ks <anandhuks1027@gmail.com>
2020-11-25 22:19:10 +0530

    quill-mention

--- src/app/quilleditor/quilleditor.component.ts
+++ src/app/quilleditor/quilleditor.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ShareDataService } from '../share-data.service';
-import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
+import { EditorChangeContent, EditorChangeSelection, QuillEditorComponent } from 'ngx-quill';
+import "quill-mention";
 @Component({
   selector: 'app-quilleditor',
   templateUrl: './quilleditor.component.html',
@@ -17,19 +18,58 @@ public bgImage;
   }
 
   changedEditor(event: EditorChangeContent | EditorChangeSelection) {
-    // tslint:disable-next-line:no-console
-    this.htmlData=event['editor']['root']['innerHTML'];
-    this.shareData.setData(this.color,this.htmlData);
+      this.htmlData=event['editor']['root']['innerHTML'];
+    // this.shareData.setData(this.color,this.htmlData);
   }
 
 
-logData(){
-  console.log(this.color);
-
-}
- 
 
   ngOnInit(): void {
   }
 
+  @ViewChild(QuillEditorComponent, { static: true }) editor: QuillEditorComponent
+  content = ''
+  matContent = ''
+
+  modules = {
+    mention: {
+      allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+      onSelect: (item, insertItem) => {
+        const editor = this.editor.quillEditor
+        insertItem(item)
+        // necessary because quill-mention triggers changes as 'api' instead of 'user'
+        editor.insertText(editor.getLength() - 1, '', 'user')
+      },
+      
+      source: (searchTerm, renderList) => {
+        const values = [
+          { id: 1, value: 'student_name' },
+          { id: 2, value: 'branch_name' },
+          {id:3,value:"city_name"},
+          {id:3,value:"teacher"},
+        ]
+
+        if (searchTerm.length === 0) {
+          renderList(values, searchTerm)
+        } else {
+          const matches = []
+
+          values.forEach((entry) => {
+            if (entry.value.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
+              matches.push(entry)
+            }
+          })
+          renderList(matches, searchTerm)
+        }
+      },
+    
+      
+    },
+    
+  }
+
+
+
+
+ 
 }
